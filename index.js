3677d24441bf14783001a36f8c16ca712051996a
twsdEv99 <xsqxqsxqsx@gmail.com>
2025-05-31 03:42:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const activePage = window.location.pathname;
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scroll;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>offset && top < offset + height ) {
+            navlinks.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header nav a[href*=' + id +']').classList.add ('active');
+           })
+        }
+    })
+}
